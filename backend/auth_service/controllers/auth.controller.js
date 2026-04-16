295c19cd4f55f992363b1e4302530629b7122f3a
bruress <hualian.my.hualian@gmail.com>
2026-04-16 13:34:19 +0300

    Add try-catch auth.controller.js

--- backend/auth_service/controllers/auth.controller.js
+++ backend/auth_service/controllers/auth.controller.js
@@ -10,63 +10,71 @@ import { cookieOptions, generateToken } from "../utils/auth.util.js";
 // req - запрашиваем все, что получили от юзера (из req.body) 
 // res - мы вернем что-нибудь
 export const registr = async (req, res) => {
-    // получаем с фронта данные
-    const {username, email, password} = req.body;
-    // если что-то пустое, то просим заполнить все
-    if (!username || !email || !password) {
-        return res.status(400).json({message: "Пожалуйста заполните все поля"});
-    };
-    // пользователь существует? (определяем по уникальному email)
-    const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-    // если он есть -> то уведомляем, что пользователь уже существует и дальнейшая регистрация бессмыслена, т.к. юзер уже есть
-    if (userExists.rows.length > 0) {
-        return res.status(400).json({message: "Пользователь уже существует"});
+    try {
+        // получаем с фронта данные
+        const {username, email, password} = req.body;
+        // если что-то пустое, то просим заполнить все
+        if (!username || !email || !password) {
+            return res.status(400).json({message: "Пожалуйста заполните все поля"});
+        };
+        // пользователь существует? (определяем по уникальному email)
+        const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        // если он есть -> то уведомляем, что пользователь уже существует и дальнейшая регистрация бессмыслена, т.к. юзер уже есть
+        if (userExists.rows.length > 0) {
+            return res.status(400).json({message: "Пользователь уже существует"});
+        }
+        // хэшируем пароль 10 солями
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // создаем нового пользователя
+        const newUser = await pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email", [username, email, hashedPassword]);
+        // генерируем jwt по айдишнику созданного пользователя
+        const token = generateToken(newUser.rows[0].id);
+        // отпарвляем токен браузеру (Set-Cookie) с текущими настройками и токеном и он возвращает его на протяжении 30 дней
+        res.cookie("token", token, cookieOptions);
+        // возвращаем созданного пользователя (он один)
+        return res.status(201).json({user: newUser.rows[0]});
+    } catch (error) {
+        return res.status(500).json({message: "Ошибка сервера при регистрации"})
     }
-    // хэшируем пароль 10 солями
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // создаем нового пользователя
-    const newUser = await pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email", [username, email, hashedPassword]);
-    // генерируем jwt по айдишнику созданного пользователя
-    const token = generateToken(newUser.rows[0].id);
-    // отпарвляем токен браузеру (Set-Cookie) с текущими настройками и токеном и он возвращает его на протяжении 30 дней
-    res.cookie("token", token, cookieOptions);
-    // возвращаем созданного пользователя (он один)
-    return res.status(201).json({user: newUser.rows[0]});
+
 };
 
 // http запрос на создание, позволяющий многим одновременно отправлять запрос на сервер (async)
 // req - запрашиваем все, что получили от юзера (из req.body) 
 // res - мы вернем что-нибудь
 export const login = async (req, res) => {
-    // получаем с фронта данные
-    const {password, email} = req.body;
-    // если чего-то не ввели, то ошибка
-    if (!password || !email) {
-        return res.status(400).json({message: "Пожалуйста заполните все поля"});
-    };
-    // получаем пользователя
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-    // такого пользователя не существуют
-    if (user.rows.length === 0) {
-        return res.status(400).json({message: "Пользователь не найден"});
-    };
-    // иначе сохраняем всю информацию о нем
-    const userData = user.rows[0];
-    // сравниваем хэш и введеный пароль
-    const isMatch = await bcrypt.compare(password, userData.password);
-    // если не совпадают -> пароль неправильный
-    if (isMatch === false) {
-        return res.status(400).json({message: "Неверный пароль"});
-    };
-    // создаем опять токен
-    const token = generateToken(userData.id);
-    // сохраняем его в браузере
-    res.cookie("token", token, cookieOptions)
+    try {
+        // получаем с фронта данные
+        const {password, email} = req.body;
+        // если чего-то не ввели, то ошибка
+        if (!password || !email) {
+            return res.status(400).json({message: "Пожалуйста заполните все поля"});
+        };
+        // получаем пользователя
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        // такого пользователя не существуют
+        if (user.rows.length === 0) {
+            return res.status(400).json({message: "Пользователь не найден"});
+        };
+        // иначе сохраняем всю информацию о нем
+        const userData = user.rows[0];
+        // сравниваем хэш и введеный пароль
+        const isMatch = await bcrypt.compare(password, userData.password);
+        // если не совпадают -> пароль неправильный
+        if (isMatch === false) {
+            return res.status(400).json({message: "Неверный пароль"});
+        };
+        // создаем опять токен
+        const token = generateToken(userData.id);
+        // сохраняем его в браузере
+        res.cookie("token", token, cookieOptions)
 
-    res.status(200).json({message: "Вход выполнен успешно",
-        user:{ id: userData.id, username: userData.username, email: userData.email}
-    })
-
+        res.status(200).json({message: "Вход выполнен успешно",
+            user:{ id: userData.id, username: userData.username, email: userData.email}
+        })
+    } catch (error) {
+        return res.status(500).json({message: "Ошибка сервера при авторизации"})
+    }
 };
 
 // http запрос на получение с удостоверением по токену (protect)
